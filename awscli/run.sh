5df552f7d48634f361f54129dd0a3966bcbe27ce
Brendon Matheson <b@mathesonventures.com>
2018-05-12 16:56:06 +0000

    Added volume for persisting AWS credentials between sessions

--- awscli/run.sh
+++ awscli/run.sh
@@ -8,6 +8,7 @@ instanceName=$name-prod
 sudo docker run -it --rm \
 	--name $instanceName \
 	--mount source=wrk,target=/dat/wrk \
+	--mount source=awscli,target=/root/.aws \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	$containerName
 
