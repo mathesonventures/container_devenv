c8f40008b86003cc6b8c6fd94ff4f4cc86278ed6
Brendon Matheson <b@mathesonventures.com>
2018-06-26 15:56:56 +0000

    Added helm as a Dockerized tool

---
+++ helm/stop.ps1
@@ -0,0 +1,16 @@
+# Copyright (c) 2018, Matheson Ventures Pte Ltd
+#
+# http://mathesonventures.com/
+#
+# This document is copyright.  You may not reproduce or transmit it any any
+# form or by any means without permission in writing from the owner of this
+# work, Matheson Ventures Pte Ltd.  If you infringe our copyright, you
+# render yourself liable for prosecution.
+
+$prefix = Get-Content container_prefix
+$name = Get-Content container_name
+$containerName = "$prefix/$name"
+$instanceName = "$($name -replace '/', '_')-prod"
+
+docker stop $instanceName
+docker rm $instanceName
