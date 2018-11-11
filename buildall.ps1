f8673cb9d5f9610b791454dcda4fc46a9167c561
Brendon Matheson <b@mathesonventures.com>
2018-11-11 14:12:20 +0700

    Added helper buildall.ps1 and cleanall.ps1 scripts

---
+++ buildall.ps1
@@ -0,0 +1,32 @@
+
+cd basedeb
+./build.ps1
+cd ..
+
+cd jdk8
+./build.ps1
+cd ..
+
+cd nodejs8
+./build.ps1
+cd ..
+
+cd nodejs10
+./build.ps1
+cd ..
+
+cd dockerdeb
+./build.ps1
+cd ..
+
+cd awscli
+./build.ps1
+cd ..
+
+cd kops
+./build.ps1
+cd ..
+
+cd terraform
+./build.ps1
+cd ..
---
+++ cleanall.ps1
@@ -0,0 +1,32 @@
+
+cd terraform
+./rmi.ps1
+cd ..
+
+cd kops
+./rmi.ps1
+cd ..
+
+cd awscli
+./rmi.ps1
+cd ..
+
+cd dockerdeb
+./rmi.ps1
+cd ..
+
+cd nodejs10
+./rmi.ps1
+cd ..
+
+cd nodejs8
+./rmi.ps1
+cd ..
+
+cd jdk8
+./rmi.ps1
+cd ..
+
+cd basedeb
+./rmi.ps1
+cd ..
