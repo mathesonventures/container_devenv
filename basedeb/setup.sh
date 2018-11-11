909c430b70fbbff976d4321bc44c2cd25b66cdce
Brendon Matheson <b@mathesonventures.com>
2018-11-11 15:01:19 +0700

    Upgraded basedeb to Debian 9.5 and adjusted locale settings to follow the "standard" configuration approach described in https://wiki.debian.org/Locale

--- basedeb/setup.sh
+++ basedeb/setup.sh
@@ -32,7 +32,6 @@ apt-get install -y \
 	vim \
 	wget
 
-echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen en_US.UTF-8
